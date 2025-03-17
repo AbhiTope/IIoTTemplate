3b08d73958a0c3f50f60121cf66e510235c41379
AbhiTope <abhijeettope.at@gmail.com>
2025-03-17 19:01:37 +0530

    .

--- backend_go/go.mod
+++ backend_go/go.mod
@@ -3,6 +3,7 @@ module iiot_template/go
 go 1.21.4
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.10.0
 	golang.org/x/crypto v0.23.0
 )
--- backend_go/utils/hashing.go
+++ backend_go/utils/hashing.go
@@ -2,7 +2,10 @@ package utils
 
 import (
 	"golang.org/x/crypto/bcrypt"
+	"time"
+	"github.com/dgrijalva/jwt-go"
 )
+
 
 func HashPassword(password string) (string, error) {
     bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
@@ -13,3 +16,20 @@ func CheckPasswordHash(password, hash string) bool {
     err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
     return err == nil
 }
+
+func CreateToken(username, role string) (string ,error){
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": username,
+		"role":     role,
+		"exp":      time.Now().Add(time.Hour * 1).Unix(), // Token expiration time
+	})
+
+	secretKey := "key"
+
+	tokenString, err := token.SignedString([]byte(secretKey))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
